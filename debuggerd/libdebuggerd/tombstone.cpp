d17562df0725738a465d1a0c1bbcc30ef4ac5f73
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add Pixeldust version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to CarbonROM
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    [myself5]: use Carbon props
    [spezi77] PD'ify
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -72,7 +72,9 @@ using namespace std::literals::string_literals;
 static void dump_header_info(log_t* log) {
   auto fingerprint = GetProperty("ro.build.fingerprint", "unknown");
   auto revision = GetProperty("ro.revision", "unknown");
+  auto pixeldust_version = GetProperty("ro.pixeldust.version", "unknown");
 
+  _LOG(log, logtype::HEADER, "Pixel Dust ROM Version: '%s'\n", pixeldust_version.c_str());
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint.c_str());
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision.c_str());
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
